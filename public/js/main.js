865e0bcbcbd7e7f0e5a389c405b19f65851a1946
Anurag Kumar <65001074+anuragkumar19@users.noreply.github.com>
2020-08-27 10:25:19 +0530

    Bugs fixed ( HTML ESCAPED, EMPTY MESSAGES )

--- public/js/main.js
+++ public/js/main.js
@@ -54,6 +54,7 @@ function outputMessage(message) {
   const div = document.createElement('div');
   div.classList.add('message');
   const p = document.createElement('p');
+  p.classList.add('meta');
   p.innerText = message.username;
   p.innerHTML += `<span>${message.time}</span>`;
   div.appendChild(p);
