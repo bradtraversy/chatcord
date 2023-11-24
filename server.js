0a60ec53b1fd007ed6c5ce6e288a8867926f6b47
ragnarbull <joel@infisical.com>
2023-11-24 11:46:21 +1100

    Fix: Redis error handling (adds reconnect retry)

--- server.js
+++ server.js
@@ -22,12 +22,60 @@ const io = socketio(server);
 app.use(express.static(path.join(__dirname, "public")));
 
 const botName = "ChatCord Bot";
+let pubClient, subClient;
 
 (async () => {
-  pubClient = createClient({ url: "redis://127.0.0.1:6379" });
-  await pubClient.connect();
-  subClient = pubClient.duplicate();
-  io.adapter(createAdapter(pubClient, subClient));
+  try {
+    pubClient = createClient({ url: "redis://127.0.0.1:6379" });
+    await pubClient.connect();
+    subClient = pubClient.duplicate();
+    io.adapter(createAdapter(pubClient, subClient));
+    console.log("Successfully connected to Redis adapter");
+  } catch (err) {
+    if (err.code === 'ECONNREFUSED') {
+      console.warn("Redis server is not available. Make sure it's running.");
+    } else {
+      console.error("Error connecting to Redis:", err);
+    }
+  }
+
+  let reconnectAttempts = 0;
+  const maxReconnectAttempts = 3;
+
+  const handleRedisError = async (err, client) => {
+    if (err && err.name === 'AbortError') {
+      console.warn(`${client === pubClient ? 'pubClient' : 'subClient'}: Redis connection aborted.`);
+    } else if (err && err.errors && err.errors.length > 0) {
+      const firstError = err.errors[0];
+      console.warn(`${client === pubClient ? 'pubClient' : 'subClient'}: ${firstError.message}`);
+    } else {
+      console.error(`${client === pubClient ? 'pubClient' : 'subClient'} error:`, err);
+    }
+
+    // Reconnect retry mechanism
+    if (reconnectAttempts < maxReconnectAttempts) {
+      reconnectAttempts++;
+      console.log(`Reconnecting ${client === pubClient ? 'pubClient' : 'subClient'} (Attempt ${reconnectAttempts} of ${maxReconnectAttempts})...`);
+      try {
+        await client.connect();
+        console.log(`Successfully reconnected to ${client === pubClient ? 'pubClient' : 'subClient'}`);
+        reconnectAttempts = 0; // resets the counter if reconnection is successfull
+      } catch (reconnectError) {
+        console.error(`Error reconnecting ${client === pubClient ? 'pubClient' : 'subClient'}:`, reconnectError);
+      }
+    } else {
+      console.error(`Max reconnect attempts reached. Shutting down the application.`);
+      process.exit(1);
+    }
+  };
+
+  pubClient.on('error', (err) => {
+    handleRedisError(err, pubClient);
+  });
+
+  subClient.on('error', (err) => {
+    handleRedisError(err, subClient);
+  });
 })();
 
 // Run when client connects
